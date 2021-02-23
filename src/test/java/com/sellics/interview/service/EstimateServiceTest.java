b7c06373e23969056b6445eed5309c7728e57313
Janardhan Sharma <janardhansharma@Janardhans-MacBook-Pro.local>
2021-02-24 02:20:19 +0530

    Test case

--- src/test/java/com/sellics/interview/service/EstimateServiceTest.java
+++ src/test/java/com/sellics/interview/service/EstimateServiceTest.java
@@ -1,7 +1,10 @@
 package com.sellics.interview.service;
 
+import com.sellics.interview.dto.RequestDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,11 +12,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 public class EstimateServiceTest {
 
+    @Autowired
+    private EstimateService estimateService;
 
     @Test
     public void testEstimateSuccess() {
         //given
+        final String searchKeyWord = "iphone";
         //when
+        int searchVolume = estimateService.estimate(RequestDto.builder().searchWord(searchKeyWord).build());
         //then
+        Assertions.assertEquals(searchVolume, 60);
     }
 }
